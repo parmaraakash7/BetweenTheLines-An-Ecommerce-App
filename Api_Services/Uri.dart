7c409d9c6d884a4fe7cd1794de32d68dadc72704
Aakash Parmar <60379362+parmaraakash7@users.noreply.github.com>
2020-08-19 14:54:55 +0530

    Add files via upload

---
+++ Api_Services/Uri.dart
@@ -0,0 +1,9 @@
+class Uri
+{
+
+  static const GET_ALL_BOOKS = "http://192.168.43.32:8081/books";
+  static const GET_ALL_WISHLIST  = "http://192.168.43.32:8081/wishlist";
+  static const GET_ALL_CART  = "http://192.168.43.32:8081/cart";
+
+  
+}
