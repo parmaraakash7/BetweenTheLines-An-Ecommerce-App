52ea517c5e95ef31228ea5c9e852f9a184737bd9
Aakash Parmar <60379362+parmaraakash7@users.noreply.github.com>
2020-08-19 14:55:57 +0530

    Update Uri.dart

--- Api_Services/Uri.dart
+++ Api_Services/Uri.dart
@@ -1,9 +1,9 @@
 class Uri
 {
 
-  static const GET_ALL_BOOKS = "http://192.168.43.32:8081/books";
-  static const GET_ALL_WISHLIST  = "http://192.168.43.32:8081/wishlist";
-  static const GET_ALL_CART  = "http://192.168.43.32:8081/cart";
+  static const GET_ALL_BOOKS = "http://192.xx.yy.zz:8081/books";
+  static const GET_ALL_WISHLIST  = "http://192.xx.yy.zz:8081/wishlist";
+  static const GET_ALL_CART  = "http://192.xx.yy.zz:8081/cart";
 
   
 }
